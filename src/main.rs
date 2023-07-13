f779e2e48c0f2a6d6ea54d7032c684452f285ba2
Alon Budker <alonio6@gmail.com>
2023-07-13 23:06:39 +0300

    add: maintain selected frame and highlight it

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crossterm::event::{self, Event as CEvent, KeyCode};
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::io;
 use std::sync::mpsc;
 use std::thread;
@@ -13,6 +13,12 @@ use tui::Terminal;
 enum Event<I> {
     Input(I),
     Tick,
+}
+
+enum ActiveSection {
+    Backlog,
+    InProgress,
+    Done,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -47,6 +53,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     terminal.clear()?;
+    let mut active_section = ActiveSection::Backlog;
 
     loop {
         terminal.draw(|canvas| {
@@ -65,25 +72,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .split(size);
 
             // For testing
-            let backlog_items = [ListItem::new("Finish styling"), ListItem::new("Eat"), ListItem::new("Go to bed")];
+            let backlog_items = [
+                ListItem::new("Finish styling"),
+                ListItem::new("Eat"),
+                ListItem::new("Go to bed"),
+            ];
             let in_progress_items = [ListItem::new("Implement logic")];
             let done_items = [ListItem::new("Get basic code running")];
             // --------
 
             let backlog = List::new(backlog_items)
-                .block(Block::default().title(" Backlog ").borders(Borders::ALL).border_type(BorderType::Double))
+                .block(
+                    Block::default()
+                        .title(" Backlog ")
+                        .borders(Borders::ALL)
+                        .border_type(
+                            match active_section {
+                                ActiveSection::Backlog => BorderType::Double,
+                                _ => BorderType::Plain
+                            }),
+                )
                 .highlight_style(Style::default())
                 .highlight_symbol(">>")
                 .style(Style::default().fg(Color::White));
 
             let inprogress = List::new(in_progress_items)
-                .block(Block::default().title(" In Progress ").borders(Borders::ALL).border_type(BorderType::Double))
+                .block(
+                    Block::default()
+                        .title(" In Progress ")
+                        .borders(Borders::ALL)
+                        .border_type(
+                            match active_section {
+                                ActiveSection::InProgress => BorderType::Double,
+                                _ => BorderType::Plain
+                            }),
+                )
                 .highlight_style(Style::default())
                 .highlight_symbol(">>")
                 .style(Style::default().fg(Color::White));
 
             let done = List::new(done_items)
-                .block(Block::default().title(" Done ").borders(Borders::ALL).border_type(BorderType::Double))
+                .block(
+                    Block::default()
+                        .title(" Done ")
+                        .borders(Borders::ALL)
+                        .border_type(
+                            match active_section {
+                                ActiveSection::Done => BorderType::Double,
+                                _ => BorderType::Plain
+                            }),
+                )
                 .highlight_style(Style::default())
                 .highlight_symbol(">>")
                 .style(Style::default().fg(Color::White));
@@ -101,10 +139,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     disable_raw_mode()?;
                     terminal.show_cursor()?;
                     break;
-                },
+                }
+                KeyCode::Char('b') => active_section = ActiveSection::Backlog,
+                KeyCode::Char('p') => active_section = ActiveSection::InProgress,
+                KeyCode::Char('d') => active_section = ActiveSection::Done,
                 _ => {}
-                
-            }
+            },
             Event::Tick => {}
         }
     }
